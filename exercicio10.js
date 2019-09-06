35853b101597b47a0fb769afb444b2f7d6db8214
caio <bladellano@gmail.com>
2019-09-06 09:38:15 -0300

    Sobre typeof

--- exercicio10.js
+++ exercicio10.js
@@ -24,7 +24,35 @@
 		subtraction(function(number1,number2){
 			return number1 - number2;
 		}) );
+
+	console.log('O resultado da multiplicação é:');
+	console.log(
+		multiplication(function(number1,number2){
+			return number1 * number2;
+		}) );
+
+	console.log('O resultado da divisão é:');
+	console.log(
+		division(function(number1,number2){
+			return number1 / number2;
+		}) );
 	
+	console.log('O resultado do resto é:');
+	console.log(
+		mod(function(number1,number2){
+			return number1 % number2;
+		}) );
+
+	//Youtube dicas.
+
+	const loading = setInterval(function(){
+		console.log('Loading...');
+	},500)
+
+	setTimeout(function(){
+		clearInterval(loading)
+		console.log('Loaded');
+	},3000)
 
 })();
 
---
+++ exercicio11.js
@@ -0,0 +1,35 @@
+
+//Wrapper objects
+var name = 'Daciuk';
+
+name.length;
+console.log("Tamanho: ", name.length);
+
+// new String; Number e Boolean
+var name1 = new String('Caio');
+console.log("name1: ", name1.valueOf());
+
+var age = new Number(30);
+age.valueOf()
+console.log("'age.valueOf()'", age.valueOf());
+
+// operador unario
+// typeof
+
+console.log("'typeof undefined'", typeof undefined); 
+console.log("'typeof {}'", typeof {}); 
+console.log("'typeof []'", typeof []); 
+console.log("'typeof null'", typeof null); 
+
+function subtract (num1, num2){
+	if(typeof num1 === 'number' && typeof num2 === 'number'){
+		return num1 - num2;
+	}
+	return 'Entre com dois números';
+}
+
+console.log( subtract(10,2) );
+console.log( subtract('JS', 2) );
+console.log( subtract({}, []) );
+
+function 
