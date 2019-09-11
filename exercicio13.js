cd2c5c529f33b474fe37f758905706a0d0778c83
Caio Silva <bladellano@gmail.com>
2019-09-10 21:30:42 -0300

    hasOwnProperty

--- exercicio13.js
+++ exercicio13.js
@@ -39,7 +39,31 @@ function moreThan( age ){
 
 console.log('The person has more than 25 yeras old?', moreThan(25));
 
+console.log('De 0 a 10:');
+
+var numbers = [];
+
+for(var i = 0; i < 20; i++){
+
+	if(i > 10){
+		break;
+	}
+	numbers.push( i );
+}
+console.log(numbers);
 
 
+console.log('Impares:');
+
+numbers = [];
+
+for(var i = 0; i <= 20; i++){
+
+	if(i % 2 === 0){
+		continue;
+	}
+	numbers.push(i);
+}
+console.log(numbers);
 
 })();//Fim
