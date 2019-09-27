6ec61d98eef9e540775e8d0244b97785d88332f6
Caio Silva <bladellano@gmail.com>
2019-09-26 23:27:09 -0300

    Sobre DOM

---
+++ main.js
@@ -0,0 +1,20 @@
+(function(win,doc){
+	'use strict';
+
+	// var del = confirm('Deseja realmente excluir?');
+
+	// if( del )
+	// 	console.log( 'Excluído com sucesso!', del );
+	// else
+	// 	console.log( 'Ação cancelada', del );
+	var $inputs = doc.getElementsByTagName('input');
+	var $inputs = doc.getElementsByTagName('input');
+	var $inputs = doc.querySelector('.nameClass');
+	var $inputs = doc.querySelectorAll('.nameClass');
+	// console.log( doc.getElementsByName('username') );
+	// console.log( doc.getElementsByTagName('input') );
+
+	console.log("$inputs", $inputs.length);
+	// console.log( doc.getElementById("my-link") ); 
+
+})(window, document);
