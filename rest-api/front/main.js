cd1736c08d7a6e11f7e1ae0e3d0434968eaca375
Caio Silva <bladellano@gmail.com>
2019-10-23 22:17:38 -0300

    concluíndo teste de rest-api

--- rest-api/front/main.js
+++ rest-api/front/main.js
@@ -1,14 +1,28 @@
 (function(){
+
     'use strict';
 
     var ajax = new XMLHttpRequest();
-    ajax.open('GET','http://localhost:3000/user/jose');
-    ajax.send();
+    ajax.open('POST','http://localhost:3000/user');
+    ajax.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+    ajax.send('username=caio&name=Caio&age=35');//No corpo, no body
 
-    ajax.addEventListener('readystatechange',function(e){
-        if(ajax.readyState === 4){
-            console.log(ajax.responseText, ajax.status);
-        } 
-    },false)
+    console.log('Cadastrando usuário...');
+    ajax.onreadystatechange = function(){
+    	if(ajax.readyState === 4){
+    		console.log('Usuário cadastrado!',ajax.responseText);    		
+    	}
+    };
+
+    var get= new XMLHttpRequest();
+    get.open('GET','http://localhost:3000/user/caio');
+    get.send();
+
+    get.onreadystatechange = function(){
+    	if(get.readyState === 4){
+    		console.log(
+    			JSON.parse(get.responseText));
+    	}
+    };
 
 })()
--- rest-api/rest-api/index.js
+++ rest-api/rest-api/index.js
@@ -2,43 +2,65 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var bodyParser = require('body-parser');
 var app = express();
 
-var users = {
-    joao:{
-        nome:'João',
-        idade:30
-    },
-    maria:{
-        nome:'Maria',
-        idade:25
-    },
-    fernando:{
-        nome:'Fernando',
-        idade:22
-    }
-}
+var users = [{
+    username:'joao',
+    name:'João',
+    age:30
+},
+{
+    username:'maria',
+    name:'Maria',
+    age:22
+}];
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(cors());
 
 app.get('/',function(req,res){
-    res.send('<h1>Home</h1>')
+    res.json({response:'Home'});
 });
 
 app.get('/user',function(req,res){
-    res.send('User');
+    res.json({response:'User'});
 });
 
 app.get('/user/:username',function(req,res){
-    
+
     var username = req.params.username;
-    if(users[username]){
-        return res.json(users[username]);
+    var hasUser = users.some(function(user){
+        return user.username === username;
+    });
+
+    if(hasUser){
+        return res.json(users.filter(function(user){
+            return user.username === username;
+        })[0]);
     }
     res.status(404).json({error:'Usuário não encontrado.'});
 }); 
 
+app.post('/user',function(req,res){
+    var username = req.body.username;
+    var age = req.body.age;
+    var name = req.body.name;
+
+    var hasUser = users.some(function(user){
+        return user.username === username;
+    });
+
+    if(!hasUser){
+       users.push({        
+        username:username,
+        name:name,
+        age:age    
+    });
+   }  
+   
+   res.json(users); 
+});
 
 app.listen(3000);;
 
